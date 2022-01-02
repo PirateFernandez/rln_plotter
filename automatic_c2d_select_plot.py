471f7cbaca3b2e87c85e46f9ccac1010bf5bfc73
Israel Fernandez <76214130+PirateFernandez@users.noreply.github.com>
2022-01-02 15:37:17 -0600

    Update automatic_c2d_select_plot.py

--- automatic_c2d_select_plot.py
+++ automatic_c2d_select_plot.py
@@ -1,12 +1,12 @@
-###python script to visualize rln4 c2d files automaticaly selected. Rln4 crates a images.npy file which ia numpy array with 3 dimensions.###
-###The first dimension is the number of c2d, the second and thrid dimensions are the images themselves as numpy array.###
-### One exaple [24,64,64]-> 24 classes with 64x64 pixel dimensions.####
+###python script to visualize rln4 c2d files automaticaly selected. Rln4 creates a images.npy file which is numpy array with 3 dimensions.###
+###The first dimension is the number of classes, the second and thrid dimensions are the images themselves as numpy arrays.###
+### One example: [24,64,64]-> 24 classes with 64x64 pixel dimensions.####
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-###first we load the nmpu array stored as .npy file in an object.###
-np_images_loaded =  np.load('/Users/israel_CUMC/Documents/python_work/numpy_files_rln4/images.npy')
-###the size of the first dimension is the number of c2d selected.###
+###first we load the nmpy array stored as .npy file in an object.###
+np_images_loaded =  np.load('/path_to/images.npy')
+###the size of the first dimension is the number of classes selected.###
 number_of_c2ds = np.size(np_images_loaded, 0)
 x_dim = np.size(np_images_loaded, 1)
 y_dim = np.size(np_images_loaded, 2)
