e21fd4bb21f0c312da03e037dbc1000db42724ba
Israel Fernandez <76214130+PirateFernandez@users.noreply.github.com>
2022-04-28 15:12:20 -0500

    Add files via upload

--- defocus_plotter_v5.py
+++ defocus_plotter_v5.py
@@ -11,7 +11,7 @@ import mrcfile
 fig = plt.figure(figsize=(15,15))
 spec = gridspec.GridSpec(ncols=8, nrows=30 , wspace=2)
 subspec1 = gridspec.GridSpecFromSubplotSpec(10, 40,subplot_spec=spec[18:,0:-2], wspace=-0.2, hspace=-0.9)
-subspec2 = gridspec.GridSpecFromSubplotSpec(24, 1,subplot_spec=spec[21:,6:], wspace=-0.5, hspace=0.0)
+subspec2 = gridspec.GridSpecFromSubplotSpec(24, 1,subplot_spec=spec[20:,6:], wspace=-0.5, hspace=0.0)
       
 #row1
 f1_ax1 = fig.add_subplot(spec[0:4,0:4])
@@ -35,7 +35,12 @@ f1_ax13 = fig.add_subplot(spec[15:19,4:6])
 f1_ax14 = fig.add_subplot(spec[15:19,6:], projection='polar')
 f1_ax14.set_yticklabels([])
 #row5
-#f1_ax15 = fig.add_subplot(subspec1[:,:])
+f1_ax15 = fig.add_subplot(spec[20:21,0:6])
+f1_ax15.set_yticklabels([])
+f1_ax15.set_xticklabels([])
+f1_ax15.set_xticks([])
+f1_ax15.set_yticks([])
+#text-box
 f1_ax16 = fig.add_subplot(subspec2[0:,0:])
 f1_ax16.set_yticklabels([])
 f1_ax16.set_xticklabels([])
@@ -99,15 +104,15 @@ def animate(i, data_x, data_y, data_x2, data_y2):
 	f1_ax12.clear()
 	f1_ax13.clear()
 	f1_ax14.clear()
-	#f1_ax15.clear()
-	#f1_ax16.clear()
+	f1_ax15.clear()
+	f1_ax16.clear()
 	#f1_ax17.clear()
 	#f1_ax18.clear()
 	#plotting row 1
 		#ax1->plot all defoci vs image number
 	f1_ax1.plot(data_x, data_y, 'o-', color='slateblue', lw=0.2, ms=2)
 	f1_ax1.set_ylabel('defocus [um]')
-	f1_ax1.set_ylim([0,3])
+	f1_ax1.set_ylim([0,6])
 	f1_ax1.tick_params(bottom=True, top=False, labelbottom=True, labeltop=False)
 	f1_ax1.set_title('image number all', y=1)
 	f1_ax1.hlines(max(data_y), 0, len(data_x), colors='blue', linestyles='dotted', label='max')
@@ -128,7 +133,7 @@ def animate(i, data_x, data_y, data_x2, data_y2):
 	f1_ax4.hlines(max(data_y[-48:]), 0, len(data_x[-48:]), colors='blue', linestyles='dotted', label='max')
 	f1_ax4.hlines(min(data_y[-48:]), 0, len(data_x[-48:]), colors='red', linestyles='dotted', label='min')
 	f1_ax4.hlines(np.mean(data_y[-48:]), 0, len(data_x[-48:]), colors='dimgray', linestyles='dashed', label='avg')
-	f1_ax4.set_ylim([0,3])
+	f1_ax4.set_ylim([0,6])
 		#ax5->plot last 32 defocus vs image number	
 	f1_ax5.plot(range(0,len(data_y[-32:])), data_y[-32:], 'o-', color='slateblue', lw=0.2, ms=2)
 	f1_ax5.set_title("last 32 imgs")
@@ -136,7 +141,7 @@ def animate(i, data_x, data_y, data_x2, data_y2):
 	f1_ax5.hlines(max(data_y[-32:]), 0, len(data_x[-32:]), colors='blue', linestyles='dotted', label='max')
 	f1_ax5.hlines(min(data_y[-32:]), 0, len(data_x[-32:]), colors='red', linestyles='dotted', label='min')
 	f1_ax5.hlines(np.mean(data_y[-32:]), 0, len(data_x[-32:]), colors='dimgray', linestyles='dashed', label='avg')
-	f1_ax5.set_ylim([0,3])
+	f1_ax5.set_ylim([0,6])
 		#ax6->plot last 16 defoci vs image number	
 	f1_ax6.plot(range(0,len(data_y[-16:])), data_y[-16:], 'o-', color='slateblue', lw=0.2, ms=2)
 	f1_ax6.set_title("last 16 imgs")
@@ -144,7 +149,7 @@ def animate(i, data_x, data_y, data_x2, data_y2):
 	f1_ax6.hlines(max(data_y[-16:]), 0, len(data_x[-16:]), colors='blue', linestyles='dotted', label='max')
 	f1_ax6.hlines(min(data_y[-16:]), 0, len(data_x[-16:]), colors='red', linestyles='dotted', label='min')
 	f1_ax6.hlines(np.mean(data_y[-16:]), 0, len(data_x[-16:]), colors='dimgray', linestyles='dashed', label='avg')
-	f1_ax6.set_ylim([0,3])
+	f1_ax6.set_ylim([0,6])
 		#ax7->polar 30% images astisgmatism	
 	f1_ax7.scatter(data_y3[pc30_data_y3:], data_x3[pc30_data_x3:], c='limegreen', s=1, alpha=0.75)
 	f1_ax7.set_title('Azymuth last 30% data', y=1)
@@ -194,8 +199,38 @@ def animate(i, data_x, data_y, data_x2, data_y2):
 	f1_ax14.scatter(data_y3[pc10_data_y3:], data_x3[pc10_data_x3:], c='limegreen', s=1, alpha=0.75)
 	f1_ax14.set_title('Azymuth last 10% data', y=1)
 	f1_ax14.set_xticklabels([])
-	####
-	f1_ax16.text(0.5,0.5, f"{len(avg_defocus)}")
+	
+	####dataset statisticks
+	#total_num_images = len(avg_defocus)
+	f1_ax16.set_xticks([])
+	f1_ax16.set_yticks([])
+	f1_ax16.text(0.25,0.94, f"Imgs corrected:", fontweight='bold')
+	f1_ax16.text(0.38,0.88, f"{len(avg_defocus)}", color='red')
+	def_belw_1u = [def_1 for def_1 in avg_defocus if def_1 < 1 ]
+	f1_ax16.text(0.04,0.8, f"Imgs def <-1uM: ", fontweight='bold')
+	f1_ax16.text(0.32,0.75, f"{len(def_belw_1u)} ({round((len(def_belw_1u)*100)/len(avg_defocus),2)}%)")
+	def_belw_2u = [def_1 for def_1 in avg_defocus if def_1 < 2 and def_1 >1 ]
+	f1_ax16.text(0.04,0.7, f"Imgs def >-1 and <-2uM:", fontweight='bold')
+	f1_ax16.text(0.32,0.65, f"{len(def_belw_2u)} ({round((len(def_belw_2u)*100)/len(avg_defocus),2)}%)")
+	def_belw_3u = [def_1 for def_1 in avg_defocus if def_1 < 3 and def_1 >2 ]
+	f1_ax16.text(0.04,0.60, f"Imgs def >2 and <-3uM:", fontweight='bold')
+	f1_ax16.text(0.32,0.55, f"{len(def_belw_3u)} ({round((len(def_belw_3u)*100)/len(avg_defocus),2)}%)")
+	###
+	res_belw_2A = [res_1 for res_1 in resolution_list_local if res_1 < 2 ]
+	res_belw_3A = [res_1 for res_1 in resolution_list_local if res_1 > 2 and res_1 < 3  ]
+	res_belw_4A = [res_1 for res_1 in resolution_list_local if res_1 > 3 and res_1 < 4 ]
+	res_belw_5A = [res_1 for res_1 in resolution_list_local if res_1 > 4 ]
+	f1_ax16.text(0.04,0.43, f"Imgs res < 2A: ", fontweight='bold')
+	f1_ax16.text(0.32,0.38, f"{len(res_belw_2A)} ({round((len(res_belw_2A)*100)/len(resolution_list_local),2)}%)")
+	def_belw_2u = [def_1 for def_1 in avg_defocus if def_1 < 2 and def_1 >1 ]
+	f1_ax16.text(0.04,0.33, f"Imgs res >2 and < 3A:", fontweight='bold')
+	f1_ax16.text(0.32,0.28, f"{len(res_belw_3A)} ({round((len(res_belw_3A)*100)/len(resolution_list_local),2)}%)")
+	def_belw_3u = [def_1 for def_1 in avg_defocus if def_1 < 3 and def_1 >2 ]
+	f1_ax16.text(0.04,0.23, f"Imgs res > 3 and < 4A:", fontweight='bold')
+	f1_ax16.text(0.32,0.18, f"{len(res_belw_4A)} ({round((len(res_belw_4A)*100)/len(resolution_list_local),2)}%)")
+	f1_ax16.text(0.04,0.13, f"Imgs res >4A:", fontweight='bold')
+	f1_ax16.text(0.32,0.08, f"{len(res_belw_5A)} ({round((len(res_belw_5A)*100)/len(resolution_list_local),2)}%)")
+	
 	####capture all jobs folders.###
 	rln_directory = os.getcwd()	
 	if 'rln_directoy/Class2D' :
@@ -232,9 +267,9 @@ def animate(i, data_x, data_y, data_x2, data_y2):
 		path_to_last = f"{rln_directory}/Class2D/{lastest_job}/run_it200_classes.mrcs"
 		path_to_last_strip = path_to_last.strip()
 		#print(f"{path_to_last_strip}")		
-		if os.path.isfile(path_to_last_strip):	
+		if os.path.isfile(path_to_last):	
 			c2d_file = f"{rln_directory}/Class2D/{lastest_job}/run_it200_classes.mrcs"
-			print(f"{path_to_last_strip}")	
+			print(f"{path_to_last}")	
 			with mrcfile.open(c2d_file) as emd:
 				nx, ny, nz = emd.header['nx'], emd.header['ny'], emd.header['nz']
 				c2d_flat = emd.data.flatten(order='F').reshape(emd.header['nx'], emd.header['ny'], emd.header['nz'])
@@ -248,10 +283,16 @@ def animate(i, data_x, data_y, data_x2, data_y2):
 					col_inx.append(i)
 			for ri, ci, i in zip(row_inx, col_inx, range(0,100)):
 				ax = fig.add_subplot(subspec1[ri:ri+2, ci:ci+2])
-				ax.imshow(c2d_flat[:,:,i], cmap='coolwarm')
+				ax.imshow(c2d_flat[:,:,i], cmap='gray')
 				ax.set_xticks([])
 				ax.set_yticks([])
-		else: 
+			###for c2d label
+			f1_ax15.text(0.425,0.4, f"C2D-{lastest_job}",fontweight='bold', fontsize='large')
+			f1_ax15.set_yticklabels([])
+			f1_ax15.set_xticklabels([])
+			f1_ax15.set_xticks([])
+			f1_ax15.set_yticks([])
+		elif len(jobs_sorted) >= 3 :
 			c2d_file = f"{rln_directory}/Class2D/{jobs_sorted[-2]}/run_it200_classes.mrcs"
 			print(f"{c2d_file}")
 
@@ -269,9 +310,43 @@ def animate(i, data_x, data_y, data_x2, data_y2):
 
 			for ri, ci, i in zip(row_inx, col_inx, range(0,100)):
 				ax = fig.add_subplot(subspec1[ri:ri+2, ci:ci+2])
-				ax.imshow(c2d_flat[:,:,i], cmap='coolwarm')
+				ax.imshow(c2d_flat[:,:,i], cmap='gray')
 				ax.set_xticks([])
 				ax.set_yticks([])
+			###for c2d label
+			f1_ax15.text(0.425,0.4, f"C2D-{jobs_sorted[-2]}", fontweight='bold', fontsize='large')
+			f1_ax15.set_yticklabels([])
+			f1_ax15.set_xticklabels([])
+			f1_ax15.set_xticks([])
+			f1_ax15.set_yticks([])
 				#
-ani = animation.FuncAnimation(fig, animate, fargs=(data_x, data_y, data_x2, data_y2), interval=1000)
+		elif  len(jobs_sorted) <= 2 :
+			c2d_file = f"{rln_directory}/Class2D/{jobs_sorted[0]}/run_it200_classes.mrcs"
+			print(f"{c2d_file}")
+
+			with mrcfile.open(c2d_file) as emd:
+				nx, ny, nz = emd.header['nx'], emd.header['ny'], emd.header['nz']
+				c2d_flat = emd.data.flatten(order='F').reshape(emd.header['nx'], emd.header['ny'], emd.header['nz'])
+			row_inx = []
+			for i in range(0,10,2):
+				for j in range(0,20):
+					row_inx.append(i)
+			col_inx = []
+			for j in range(10):
+				for i in range(0,40,2):
+					col_inx.append(i)
+
+			for ri, ci, i in zip(row_inx, col_inx, range(0,100)):
+				ax = fig.add_subplot(subspec1[ri:ri+2, ci:ci+2])
+				ax.imshow(c2d_flat[:,:,i], cmap='gray')
+				ax.set_xticks([])
+				ax.set_yticks([])
+			f1_ax15.text(0.425,0.4, f"C2D-{jobs_sorted[0]}",fontweight='bold', fontsize='large')
+			f1_ax15.set_yticklabels([])
+			f1_ax15.set_xticklabels([])
+			f1_ax15.set_xticks([])
+			f1_ax15.set_yticks([])
+		
+				#
+ani = animation.FuncAnimation(fig, animate, fargs=(data_x, data_y, data_x2, data_y2), interval=10000)
 plt.show()	
